2b487315309c2c3ebddda3a535680cef4845d63e
Daniel <137360936+danielmrz-dev@users.noreply.github.com>
2025-01-07 16:11:57 -0300

    Arruma responsividade do formulário

--- src/app/components/user-register-form/user-register-form-controller.ts
+++ src/app/components/user-register-form/user-register-form-controller.ts
@@ -8,7 +8,24 @@ export class UserRegisterFormController {
 
     constructor() {
         this.userRegisterForm = this._fb.group({
-            nome: ['', [Validators.required]]
+            cpf: [5, [Validators.required]],
+            name: ['D', [Validators.required]],
+            lastName: ['M', [Validators.required]],
+            email: ['M', [Validators.required]],
+            phoneNumber: ['M', [Validators.required]],
+            birthDate: ['M', [Validators.required]],
+            responsible: ['M', [Validators.required]],
+            host: ['M', [Validators.required]],
+            address: this._fb.group({
+                cep: ['M', [Validators.required]],
+                logradouro: ['M', [Validators.required]],
+                numero: ['M', [Validators.required]],
+                complemento: ['M', [Validators.required]],
+                bairro: ['M', [Validators.required]],
+                localidade: ['M', [Validators.required]],
+                uf: ['M', [Validators.required]],
+            }),
+
         })
     }
 
--- src/app/components/user-register-form/user-register-form.component.ts
+++ src/app/components/user-register-form/user-register-form.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { UserRegisterFormController } from './user-register-form-controller';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 
 
@@ -23,5 +24,13 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './user-register-form.component.scss'
 })
 export class UserRegisterFormComponent extends UserRegisterFormController {
+  stepperOrientation: 'horizontal' | 'vertical' = 'vertical';
+  private readonly _breakpointObserver = inject(BreakpointObserver)
 
+  constructor() {
+    super();
+    this._breakpointObserver.observe([Breakpoints.HandsetPortrait]).subscribe(result => {
+      this.stepperOrientation = result.matches ? 'vertical' : 'horizontal';
+    });
+  }
 }
